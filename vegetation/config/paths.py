085392d01daae9e22a5e61b69a6df0105a5af7a1
GondekNP <npg@berkeley.edu>
2025-01-09 19:42:11 +0000

    annoying setup to make pixi env work with github runner paths and whatnot. Definitely a cleaner way is to just use the docker image we build here locally, push it to registry, and re-use it - but that's more work ATM and we can address later

--- vegetation/config/paths.py
+++ vegetation/config/paths.py
@@ -1,6 +1,17 @@
-INITIAL_AGENTS_PATH = "/workspaces/mesa_abm_poc/vegetation/data/initial_agents.json"
+import os
+
+## GLOBALS - these should not change between runtime environment
 DEM_STAC_PATH = "https://planetarycomputer.microsoft.com/api/stac/v1/"
-LOCAL_STAC_CACHE_FSTRING = (
-    "/workspaces/mesa_abm_poc/.local_dev_data/{band_name}_{bounds_md5}.tif"
+
+## LOCALS - to be used with a .env file, but some defaults are provided, since
+## the devcontainer should make this standard. The one environment outside of the
+## devcontainer is the github actions runner, so we provide this to override the defaults.
+INITIAL_AGENTS_PATH = os.getenv(
+    "INITIAL_AGENTS_PATH",
+    "/workspaces/mesa_abm_poc/vegetation/data/initial_agents.json",
 )
-SAVE_LOCAL_STAC_CACHE = True
+LOCAL_STAC_CACHE_FSTRING = os.getenv(
+    "LOCAL_STAC_CACHE_FSTRING",
+    "/workspaces/mesa_abm_poc/.local_dev_data/{band_name}_{bounds_md5}.tif",
+)
+SAVE_LOCAL_STAC_CACHE = os.getenv("SAVE_LOCAL_STAC_CACHE", True)
