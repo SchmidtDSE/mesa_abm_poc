3e0e7d892d00556af0a1e672fe2242aea0f7ff4c
GondekNP <npg@berkeley.edu>
2025-01-09 16:55:33 +0000

    okay almost there, just copy the contents not the folder

--- .devcontainer/scripts/transfer_local_dev_cache.sh
+++ .devcontainer/scripts/transfer_local_dev_cache.sh
@@ -4,4 +4,4 @@ if [ ! -d "/workspaces/mesa_abm_poc/.local_dev_data" ]; then
     mkdir /workspaces/mesa_abm_poc/.local_dev_data
 fi
 
-cp -r /local_dev_data /workspaces/mesa_abm_poc/.local_dev_data
+cp -r /local_dev_data/* /workspaces/mesa_abm_poc/.local_dev_data
