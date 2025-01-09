871b9c1f09b58b5d559d4f874e7cad83b91ac863
GondekNP <npg@berkeley.edu>
2025-01-09 16:59:18 +0000

    working transfer of cache on build - change fstring path to reflect

--- vegetation/config/paths.py
+++ vegetation/config/paths.py
@@ -1,4 +1,4 @@
 INITIAL_AGENTS_PATH = "/workspaces/mesa_abm_poc/vegetation/data/initial_agents.json"
 DEM_STAC_PATH = "https://planetarycomputer.microsoft.com/api/stac/v1/"
-LOCAL_STAC_CACHE_FSTRING = "/local_dev_data/{band_name}_{bounds_md5}.tif"
+LOCAL_STAC_CACHE_FSTRING = "/workspaces/mesa_abm_poc/.local_dev_data/{band_name}_{bounds_md5}.tif"
 SAVE_LOCAL_STAC_CACHE = True
