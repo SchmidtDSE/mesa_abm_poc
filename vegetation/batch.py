5f6f539d66d7bc15d49c8b2e0a91b5b4b31a01f0
GondekNP <npg@berkeley.edu>
2025-01-09 05:21:35 +0000

    add batch run

---
+++ vegetation/batch.py
@@ -0,0 +1,23 @@
+from mesa.batchrunner import batch_run
+from patch.model import Vegetation
+from numpy import arange
+
+TST_JOTR_BOUNDS = [-116.326332, 33.975823, -116.289768, 34.004147]
+
+model_params = {
+    "num_steps": 100,
+    "management_planting_density": arange(0, 1, .05),
+    "export_data": False,
+    "bounds": TST_JOTR_BOUNDS,
+}
+
+if __name__ == '__main__':
+    results = batch_run(
+        Vegetation,
+        parameters=model_params,
+        iterations=5,
+        max_steps=100,
+        number_processes=None,
+        data_collection_period=1,
+        display_progress=True,
+    )
