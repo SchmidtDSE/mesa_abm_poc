bdaef3e16166488fc01ec1b7d86b6f27a47470ab
GondekNP <npg@berkeley.edu>
2025-01-16 16:47:22 +0000

    attempt to use git autocomplete dynamically reading version

--- .devcontainer/scripts/on_build/install_git_and_ssh.sh
+++ .devcontainer/scripts/on_build/install_git_and_ssh.sh
@@ -1,3 +1,10 @@
 #/bin/bash
 
-apt-get update && apt-get install -y git openssh-client
+apt-get update && apt-get install -y git openssh-client curl bash-completion
+
+# Get installed git version and remove 'git version ' prefix
+GIT_VERSION=$(git --version | cut -d' ' -f3)
+
+# Configure git autocompletion for branches using detected version
+curl "https://raw.githubusercontent.com/git/git/v${GIT_VERSION}/contrib/completion/git-completion.bash" -o /tmp/.git-completion.bash
+source /tmp/.git-completion.bash
