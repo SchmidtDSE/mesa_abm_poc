fabb2bee40144a25a8a903477ffe3f35d07d0aa1
GondekNP <npg@berkeley.edu>
2025-01-16 16:53:53 +0000

    working(?) bash completion for git - going to check w/ rebuild

--- .devcontainer/scripts/on_build/install_git_and_ssh.sh
+++ .devcontainer/scripts/on_build/install_git_and_ssh.sh
@@ -2,9 +2,5 @@
 
 apt-get update && apt-get install -y git openssh-client curl bash-completion
 
-# Get installed git version and remove 'git version ' prefix
-GIT_VERSION=$(git --version | cut -d' ' -f3)
-
-# Configure git autocompletion for branches using detected version
-curl "https://raw.githubusercontent.com/git/git/v${GIT_VERSION}/contrib/completion/git-completion.bash" -o /tmp/.git-completion.bash
-source /tmp/.git-completion.bash
+# To enable bash completion for git
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
