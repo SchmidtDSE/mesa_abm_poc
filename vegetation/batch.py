ae02819340c980d6d937c567eda1507b7995e272
lucialayr <lucia.sophie.layritz@posteo.de>
2025-01-22 00:08:36 +0000

    interactive input works. still saves inside the container.

--- vegetation/batch.py
+++ vegetation/batch.py
@@ -9,7 +9,15 @@ from vegetation.config.paths import (
 
 import pandas as pd
 
-run_tag = 'test'
+run_steps = input("Setting up a simulation. Please enter the number of steps you want to simulate: ")
+print(f'Simulating {run_steps} steps')
+
+run_iter = input("Enter the number of model iterations you want to simulate: ")
+print(f'Simulating {run_iter} iterations')
+
+run_name = input("Enter the name of your simulation: ")
+print(f'Saving results to local_dev_data/results/{run_name}.')
+
 
 # TODO: Implement early stopping when all the JOTR die off
 # Issue URL: https://github.com/SchmidtDSE/mesa_abm_poc/issues/18
@@ -27,11 +35,11 @@ if __name__ == "__main__":
     results = batch_run(
         Vegetation,
         parameters=model_params,
-        iterations=5,
-        max_steps=3,
+        iterations=int(run_iter),
+        max_steps=int(run_steps),
         number_processes=1,
         data_collection_period=1,
         display_progress=True,
     )
 
-    pd.DataFrame(results).to_csv(f'vegetation/.local_dev_data/results/{run_tag}')
+    pd.DataFrame(results).to_csv(f'vegetation/.local_dev_data/results/{run_name}')
