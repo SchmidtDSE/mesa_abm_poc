5e9059b299d6241fb880920e8cf9261c59c8b27f
GondekNP <npg@berkeley.edu>
2025-01-22 15:45:34 -0800

    do I need sudo here?

--- .devcontainer/scripts/post_create/chown_to_joyvan.sh
+++ .devcontainer/scripts/post_create/chown_to_joyvan.sh
@@ -1,1 +1,3 @@
-sudo chown -R joyvan:joyvan /workspaces
+# chown the /workspaces directory to joyvan, according to 
+# the user that is running the container in the jupyterverse
+chown -R joyvan:joyvan /workspaces
