a5bf7f6b03f586aca71cd42cc7c188b2994dab29
GondekNP <npg@berkeley.edu>
2025-01-23 00:03:11 +0000

    delete .pixi if it already exists to try and avoid issues with rebuilding and keeping the same volume

--- .devcontainer/scripts/post_create/verify_or_install_pixi_env.sh
+++ .devcontainer/scripts/post_create/verify_or_install_pixi_env.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# if /workspaces/mesa_abm_poc/.pixi exists, delete its contents
+# this should not be triggered unless we rebuild the container 
+# without cache
+if [ -d "/workspaces/mesa_abm_poc/.pixi" ]; then
+  rm -rf /workspaces/mesa_abm_poc/.pixi/*
+fi
+
 pixi install
