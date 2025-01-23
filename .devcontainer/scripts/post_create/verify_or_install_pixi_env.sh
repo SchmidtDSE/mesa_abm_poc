ca5ea5330931120e772cb0c091f5ecc49a1faf05
GondekNP <npg@berkeley.edu>
2025-01-23 16:56:41 +0000

    Okay - joyvan's got password-less root now and we are able to chown .pixi

--- .devcontainer/scripts/post_create/verify_or_install_pixi_env.sh
+++ .devcontainer/scripts/post_create/verify_or_install_pixi_env.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# Ensure that the jovyan user owns the .pixi directory, so we 
+# can delete its contents if necessary
+sudo chown -R jovyan:root .pixi
+
 # if /workspaces/mesa_abm_poc/.pixi exists, delete its contents
 # this should not be triggered unless we rebuild the container 
 # without cache
 if [ -d "/workspaces/mesa_abm_poc/.pixi" ]; then
   rm -rf /workspaces/mesa_abm_poc/.pixi/*
 fi
-
-# Ensure that the jovyan user owns the .pixi directory
-sudo chown -R jovyan:root .pixi
 
 # Install pixi dependencies according to pixi.toml and pixi.lock
 pixi install
