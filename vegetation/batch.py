60103340d92f12613e97de0578bc68dee692022f
GondekNP <npg@berkeley.edu>
2025-01-24 23:57:41 +0000

    refactor to try with argparse w/ defaults

--- vegetation/batch.py
+++ vegetation/batch.py
@@ -1,43 +1,55 @@
-from mesa.batchrunner import batch_run
-from vegetation.patch.model import Vegetation
-from numpy import arange
-from vegetation.config.paths import (
-    LOCAL_STAC_CACHE_FSTRING,
-    SAVE_LOCAL_STAC_CACHE,
-    DEM_STAC_PATH,
-)
+import argparse
+import os
 
 import pandas as pd
-import os as os
+from mesa.batchrunner import batch_run
+from numpy import arange
 
-run_steps = input("Setting up a simulation. Please enter the number of steps you want to simulate: ")
-print(f'Simulating {run_steps} steps')
+from vegetation.patch.model import Vegetation
 
-run_iter = input("Enter the number of model iterations you want to simulate: ")
-print(f'Simulating {run_iter} iterations')
 
-run_name = ""
-while True:
-    run_name = input("Enter the name of your simulation: ")
-    print(f'Saving results to vegetation/.local_dev_data/results/{run_name}.')
+def get_interactive_params():
+    run_steps = input("Please enter the number of steps you want to simulate: ")
+    print(f"Simulating {run_steps} steps")
 
-    if os.path.exists(f'vegetation/.local_dev_data/results/{run_name}'):
-        overwrite = input("That name already exists. Do you want to overwrite? (y/n) ")
-        if overwrite == 'n':
-            continue 
-        elif overwrite == 'y':
-            break 
+    run_iter = input("Enter the number of model iterations you want to simulate: ")
+    print(f"Simulating {run_iter} iterations")
+
+    while True:
+        run_name = input("Enter the name of your simulation: ")
+        output_path = f"vegetation/.local_dev_data/results/{run_name}"
+        print(f"Saving results to {output_path}")
+
+        if os.path.exists(output_path):
+            overwrite = input(
+                "That name already exists. Do you want to overwrite? (y/n) "
+            )
+            if overwrite.lower() == "y":
+                break
+            elif overwrite.lower() == "n":
+                continue
         else:
-            print("Invalid input. Please enter 'y' or 'n'.")
-    else:
-        break 
+            break
+
+    return run_steps, run_iter, run_name
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run vegetation simulation")
+    parser.add_argument(
+        "--interactive", action="store_true", help="Run in interactive mode"
+    )
+    parser.add_argument("--steps", type=int, help="Number of simulation steps")
+    parser.add_argument("--iterations", type=int, help="Number of model iterations")
+    parser.add_argument("--name", type=str, help="Simulation name")
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        default=False,
+        help="Overwrite existing results",
+    )
+    return parser.parse_args()
 
-
-
-# TODO: Implement early stopping when all the JOTR die off
-# Issue URL: https://github.com/SchmidtDSE/mesa_abm_poc/issues/18
 
 TST_JOTR_BOUNDS = [-116.326332, 33.975823, -116.289768, 34.004147]
 
@@ -49,14 +61,35 @@ model_params = {
 }
 
 if __name__ == "__main__":
+    args = parse_args()
+
+    if args.interactive:
+        run_steps, run_iter, run_name = get_interactive_params()
+    else:
+        if not all([args.steps, args.iterations, args.name]):
+            raise ValueError(
+                "In non-interactive mode, --steps, --iterations, and --name are required"
+            )
+
+        output_path = f"vegetation/.local_dev_data/results/{args.name}"
+        if os.path.exists(output_path) and not args.overwrite:
+            raise ValueError(
+                f"Output path {output_path} exists. Use --force to overwrite"
+            )
+
+        run_steps = int(args.steps)
+        run_iterations = int(args.iterations)
+        run_name = args.name
+
     results = batch_run(
         Vegetation,
         parameters=model_params,
-        iterations=int(run_iter),
-        max_steps=int(run_steps),
+        iterations=run_iterations,
+        max_steps=run_steps,
         number_processes=1,
         data_collection_period=1,
         display_progress=True,
     )
 
-    pd.DataFrame(results).to_csv(f'vegetation/.local_dev_data/results/{run_name}')
+    output_path = f"vegetation/.local_dev_data/results/{run_name}"
+    pd.DataFrame(results).to_csv(output_path)
