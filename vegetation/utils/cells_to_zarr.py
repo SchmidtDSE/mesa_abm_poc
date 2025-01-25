48b506f2724f419f7f67b8692b0b46a79d0b616d
GondekNP <npg@berkeley.edu>
2025-01-25 02:26:42 +0000

    initial attempt at ZarrManager class

--- vegetation/utils/cells_to_zarr.py
+++ vegetation/utils/cells_to_zarr.py
@@ -1,43 +1,79 @@
 import numpy as np
 import zarr
-from typing import List, Tuple, Optional
+from typing import List, Dict, Any, Tuple
+import json
+import hashlib
 from vegetation.patch.space import VegCell
 
 
-def initialize_synchronized_zarr(grid_shape, filename, crs=None, transformer_json=None):
-    """Initialize a thread-safe Zarr array for multiple parallel simulations"""
+class ZarrManager:
+    def __init__(
+        self, grid_shape, max_timestep, filename, crs=None, transformer_json=None
+    ):
+        self.grid_shape = grid_shape
+        self.x_dim, self.y_dim = grid_shape
 
-    store = zarr.DirectoryStore(filename)
-    sync_store = zarr.ProcessSynchronizer(filename + ".sync")
+        self.max_timestep = max_timestep
+        self.filename = filename
+        self.crs = crs
+        self.transformer_json = transformer_json
 
-    return zarr.create(
-        shape=(0, 0, grid_shape[0], grid_shape[1]),  # (sim_id, time, x, y)
-        chunks=(1, 1, grid_shape[0], grid_shape[1]),
-        dtype=np.int8,
-        store=store,
-        synchronizer=sync_store,
-        extendable=(True, True, False, False),  # allow extending sim_id and time
-    )
+        self._initialize_zarr_store(filename)
+        self._initialize_synchronizer(filename)
+        self._initialize_zarr_root_group()
 
+    def _initialize_zarr_store(self, filename):
+        self._zarr_store = zarr.DirectoryStore(filename)
 
-def append_synchronized_timestep(
-    zarr_array: zarr.Array, sim_idx: int, timestep_idx: int, timestep_array: np.ndarray
-) -> None:
-    """Thread-safe append of simulation timestep"""
+    def _initialize_synchronizer(self, filename):
+        self._synchronizer = zarr.ProcessSynchronizer(filename + ".sync")
 
-    if sim_idx >= zarr_array.shape[0]:
-        zarr_array.resize(sim_idx + 1, axis=0)
-    if zarr_array.shape[1] <= timestep_idx:
-        zarr_array.resize(timestep_idx + 1, axis=1)
+    def _initialize_zarr_root_group(self):
+        self._zarr_root_group = zarr.group(
+            store=self.zarr_store, synchronizer=self._synchronizer
+        )
 
-    zarr_array[sim_idx, timestep_idx] = timestep_array
+    def initialize_zarr_group(
+        filename: str,
+    ) -> zarr.Group:
+        store = zarr.DirectoryStore(filename)
+        sync_store = zarr.ProcessSynchronizer(filename + ".sync")
 
+        return zarr.group(store=store, synchronizer=sync_store)
 
-def get_array_from_nested_cell_list(veg_cells: List[List[VegCell]]) -> np.ndarray:
-    """Get a numpy array from a list of lists of VegCell objects"""
+    def get_run_parameter_hash(run_parameters: Dict[str, Any]) -> str:
+        param_str = json.dumps(run_parameters, sort_keys=True)
+        return hashlib.sha256(param_str.encode()).hexdigest()
 
-    def safe_get_stage(cell: VegCell) -> int:
-        return -1 if cell.jotr_max_life_stage is None else cell.jotr_max_life_stage
+    def append_synchronized_timestep(
+        self,
+        run_parameters: Dict[str, Any],
+        timestep_idx: int,
+        replicate_idx: int,
+        timestep_array: np.ndarray,
+    ) -> None:
+        param_hash = self.get_run_parameter_hash(run_parameters)
 
-    veg_array = np.array([[safe_get_stage(cell) for cell in row] for row in veg_cells])
-    return veg_array
+        if param_hash not in self._zarr_root_group:
+
+            param_hash_group = self._zarr_root_group.create_group(param_hash)
+
+            sim_array = param_hash_group.create_dataset(
+                param_hash,
+                shape=(0, self.max_timestep, self.x_dim, self.y_dim),  # 0 replicates
+                chunks=(1, self.x_dim, self.y_dim),
+                dtype=np.int8,
+            )
+
+            sim_array.attrs["run_parameters"] = run_parameters
+
+        self._zarr_root_group[param_hash][replicate_idx][timestep_idx] = timestep_array
+
+    def get_array_from_nested_cell_list(veg_cells: List[List[VegCell]]) -> np.ndarray:
+        def safe_get_stage(cell: VegCell) -> int:
+            return -1 if cell.jotr_max_life_stage is None else cell.jotr_max_life_stage
+
+        veg_array = np.array(
+            [[safe_get_stage(cell) for cell in row] for row in veg_cells]
+        )
+        return veg_array
